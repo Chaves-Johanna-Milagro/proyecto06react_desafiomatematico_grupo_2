c2fab8c422ac636de5587f0ca912ef179ccc512d
Chaves-Johanna-Milagro <165029467+Chaves-Johanna-Milagro@users.noreply.github.com>
2024-10-18 15:25:59 -0300

    input y botones basicos
    se instaló react-boostrap y se programó la entrada de las respuestas y los botonea para verificar el resultado y pasar al siguiente desafío.
    no poseen funcionalidad solo es un básico

---
+++ ProyectReactGrupo2/src/Components/InputAnswer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function InputAnswer() {
+  return (
+    <>
+    <div>
+      <input type='number'placeholder="Respuesta"></input>
+      <button>Verificar Resultado</button>
+      <button>Siguente Desafio</button>
+    </div>
+    </>
+  );
+}
+
+export default InputAnswer
--- ProyectReactGrupo2/src/main.jsx
+++ ProyectReactGrupo2/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import InputAnswer from './Components/InputAnswer.jsx'
 
 createRoot(document.getElementById('root')).render(
+  //    <App />
   <StrictMode>
-    <App />
+
+    <InputAnswer />
   </StrictMode>,
 )
