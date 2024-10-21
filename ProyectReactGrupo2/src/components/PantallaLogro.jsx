c634725c4514ca18e753216dc3451c92b411cbea
Josepheum <30935971+Josepheum@users.noreply.github.com>
2024-10-20 23:11:51 -0300

    Agregado de puntaje y ejercicios pendientes, pantallas bases Logro y derrota no vinculadas

---
+++ ProyectReactGrupo2/src/components/PantallaLogro.jsx
@@ -0,0 +1,10 @@
+function PantallaLogro(){
+    return(
+        <div>
+        <h1>SUPERASTE EL DESAFÍO MATEMÁTICO</h1>
+        <p>Presione el botón para empezar</p>
+      </div>
+    )
+}
+
+export default PantallaLogro;
