bff5d7c216df2183ff8bfd4ae0a74fb8bd1b52b5
Josepheum <30935971+Josepheum@users.noreply.github.com>
2024-10-22 14:45:35 -0300

    Actualización PantallaLogro - Mensaje de felicitación añadido

--- ProyectReactGrupo2/src/components/PantallaLogro.jsx
+++ ProyectReactGrupo2/src/components/PantallaLogro.jsx
@@ -2,6 +2,8 @@ function PantallaLogro(){
     return(
         <div>
         <h1>SUPERASTE EL DESAFÍO MATEMÁTICO</h1>
+        <h2>¡Grandioso!</h2>
+        <h3>Sigue estudiando y buscando retos más dificultosos</h3>
         <p>Presione el botón para empezar</p>
       </div>
     )
