944ea589b7acd274c60218b17b6faeef72f8030b
Josepheum <30935971+Josepheum@users.noreply.github.com>
2024-10-22 14:46:44 -0300

    Actualización PantallaDerrota - Añade mensaje de consejo

--- ProyectReactGrupo2/src/components/PantallaDerrota.jsx
+++ ProyectReactGrupo2/src/components/PantallaDerrota.jsx
@@ -2,6 +2,7 @@ function PantallaDerrota(){
     return(
         <div>
         <h1>NO HAS SUPERADO EL DESAFÍO MATEMÁTICO</h1>
+        <h3>Deberías ejercitar más tus habilidades matemáticas</h3>
         <p>Presione el botón para empezar</p>
       </div>
     )
